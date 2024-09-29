64321b59c720ff217668d1df8cacfc5792b03edf
Sekou Samassa <sksamassa@gmail.com>
2024-09-29 21:49:32 +0500

    add a reusable table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,3 +1,22 @@
-export default function Table() {
-  return <div>Table</div>
+export default function Table({ data, config }) {
+  const renderedRows = data.map((fruit, index) => {
+    const renderedCells = config.map((column) => {
+      return <td className="p-3" key={column.label}>{column.render(fruit)}</td>;
+    });
+
+    return <tr key={index} className="border-b">{renderedCells}</tr>;
+  });
+
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,6 +1,23 @@
 import Table from "../components/Table";
 
+const data = [
+  { name: "Orange", color: "bg-orange-500", score: 5 },
+  { name: "Apple", color: "bg-red-500", score: 5 },
+  { name: "Banana", color: "bg-yellow-500", score: 4 },
+  { name: "Lime", color: "bg-green-500", score: 4 },
+];
+
+const config = [
+  { label: "Name", render: (rowData) => rowData.name },
+  {
+    label: "Color",
+    render: (rowData) => <div className={`p-3 border-2 ${rowData.color}`}></div>,
+  },
+  { label: "Score", render: (rowData) => rowData.score,
+    
+   },
+];
 
 export default function TablePage() {
-  return <Table />
+  return <Table data={data} config={config} />;
 }
